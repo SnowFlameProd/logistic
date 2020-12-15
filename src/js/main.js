4326c3c85aeca1e06510ea6477a0bdd9aa0a0a25
SnowFlame <genylep@mail.ru>
2020-12-15 15:37:41 +0300

    сделал ScrollSpy для навигации и подключил модальное окно на главной странице

---
+++ src/js/main.js
@@ -0,0 +1,114 @@
+$(function() {
+  let intro = $("#intro");
+  let header = $("#header");
+  let introH = intro.innerHeight();
+  let headerH = header.innerHeight();
+  let scrollTop = $(window).scrollTop();
+
+  // Header
+
+  headerScroll();
+
+  $(window).on("scroll resize", function() {
+    headerScroll();
+  });
+
+  function headerScroll() {
+    introH = intro.innerHeight();
+    headerH = header.innerHeight();
+    let scrollTop = $(this).scrollTop();
+
+    if (scrollTop >= introH - headerH) {
+      header.addClass("header_scroll");
+    } else {
+      header.removeClass("header_scroll");
+    }
+  }
+
+  // smooth scroll
+
+  $("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+
+    let scrollElement = $(this).data("scroll");
+    let scrollElementPos = $(scrollElement).offset().top;
+
+    $("html, body").animate({
+      scrollTop: scrollElementPos - headerH
+    }, 500)
+  });
+
+  // scroll spy
+
+  let windowH = $(window).height();
+  scrollSpy(scrollTop);
+
+  $(window).on("scroll", function() {
+    scrollTop = $(this).scrollTop();
+    scrollSpy(scrollTop);
+  });
+
+  function scrollSpy(scrollTop) {
+    $("[data-scrollspy]").each(function() {
+      let $this = $(this);
+      let sectionId = $this.data('scrollspy');
+      let sectionOffset = $this.offset().top;
+      sectionOffset = sectionOffset - (windowH * 0.33333);
+
+      if(scrollTop >= sectionOffset) {
+        $('#nav [data-scroll]').removeClass('active');
+        $('#nav [data-scroll="'+sectionId+'"]').addClass('active');
+      }
+
+      if(scrollTop == 0) {
+        $('#nav [data-scroll]').removeClass('active');
+      }
+    });
+  }
+
+  // Modal
+
+  $("[data-modal]").on("click", function(event) {
+    event.preventDefault();
+    let modal = $(this).data("modal");
+
+    $("body").addClass("no-scroll");
+    $(modal).addClass("show");
+
+    setTimeout(function() {
+      $(modal).find(".modal__content").css({
+        transform: "scale(1)",
+        opacity: "1"
+      });
+    });
+  });
+
+  $("[data-modal-close]").on("click", function(event) {
+    event.preventDefault();
+    let modal = $(this).parents(".modal");
+    
+    modalClose(modal);
+  });
+
+  $(".modal").on("click", function() {
+    let modal = $(this);
+
+    modalClose(modal);
+  })
+
+  $(".modal__content").on("click", function(event) {
+    event.stopPropagation();
+  })
+
+  function modalClose(modal) {
+    modal.find(".modal__content").css({
+      transform: "scale(.5)",
+      opacity: "0"
+    });
+
+    setTimeout(function() {
+      $("body").removeClass("no-scroll");
+      modal.removeClass("show");
+    }, 200);
+  };
+});
