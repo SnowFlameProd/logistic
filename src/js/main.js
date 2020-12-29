7e8798462f9d75262a8d49f93b6c390d62a3e058
SnowFlame <genylep@mail.ru>
2020-12-29 18:24:47 +0300

    подключил бургер на мобильных устройствах, добавил анимацию некоторым блокам с помощью aos

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,25 @@
 $(function() {
+  // nav toggle on mobile
+
+  let navToggle = $("#navToggle");
+  let nav = $("#nav");
+
+  navToggle.on("click", function(event) {
+    event.preventDefault();
+    $(this).toggleClass("active");
+    nav.toggleClass("show");
+    $("body").toggleClass("no-scroll");
+    if (scrollTop <= introH - headerH) {
+      header.toggleClass("header_scroll");
+    }
+  });
+
+  $(window).on("resize", function() {
+    navToggle.removeClass("active");
+    nav.removeClass("show");
+    $("body").removeClass("no-scroll");
+  });
+
   let intro = $("#intro");
   let header = $("#header");
   let introH = intro.innerHeight();
@@ -32,6 +53,10 @@ $(function() {
 
     let scrollElement = $(this).data("scroll");
     let scrollElementPos = $(scrollElement).offset().top;
+
+    navToggle.removeClass("active");
+    nav.removeClass("show");
+    $("body").removeClass("no-scroll");
 
     $("html, body").animate({
       scrollTop: scrollElementPos - headerH
@@ -80,7 +105,7 @@ $(function() {
         transform: "scale(1)",
         opacity: "1"
       });
-    });
+    }, 100);
   });
 
   $("[data-modal-close]").on("click", function(event) {
@@ -126,7 +151,7 @@ $(function() {
     fade: true,
     autoplay: true,
     autoplaySpeed: 8000,
-    speed: 600
+    speed: 1000
   });
   
   $("#introSliderPrev").on("click", function() {
@@ -148,5 +173,30 @@ $(function() {
     arrows: false,
     dots: true,
     speed: 500
+  });
+
+  // Aos.js https://github.com/michalsnik/aos
+
+  AOS.init({
+    // Global settings:
+    disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 80, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 700, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
   });
 });
